446b0a5091cd3fa6b5dc8205ef097baf0c5cc2af
Sergej Garkusha <Tenebrann@googlemail.com>
2021-06-13 03:20:36 +0200

    Todo-list 1.1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import Todolist, { TasksType } from './components/modules/Todolist/Todolist';
-
 import './App.css';
+import { v1 } from 'uuid';
 
 export type KeyType = 'all' | 'complited' | 'active';
 
 function App() {
   let [tasks1, setTasks1] = useState<Array<TasksType>>([
-    { id: 1, title: 'CSS', isDone: true },
-    { id: 2, title: 'JS', isDone: true },
-    { id: 3, title: 'React', isDone: false },
-    { id: 4, title: 'Redux', isDone: false },
+    { id: v1(), title: 'CSS', isDone: true },
+    { id: v1(), title: 'JS', isDone: true },
+    { id: v1(), title: 'React', isDone: false },
+    { id: v1(), title: 'Redux', isDone: false },
   ]);
 
   let [filterTask, setfilterTask] = useState<KeyType>('all');
@@ -27,10 +27,27 @@ function App() {
     setfilterTask(changeValue);
   }
 
-  function removeTask(id: number) {
+  function removeTask(id: string) {
     let tasks = tasks1.filter(list => list.id !== id);
 
     setTasks1(tasks);
+  }
+
+  function addTask(newValue: string) {
+    let newTask = { id: v1(), title: newValue, isDone: false };
+
+    setTasks1([newTask, ...tasks1]);
+  }
+
+  function changeStatus(id: string, isDone: boolean) {
+    let task = tasks1.find(list => list.id === id);
+    if (task) {
+      task.isDone = isDone;
+    }
+
+    let copy = [...tasks1];
+
+    setTasks1(copy);
   }
 
   return (
@@ -40,6 +57,9 @@ function App() {
         tasks={taskTodolist}
         removeTask={removeTask}
         changeFilter={changeFilter}
+        addTask={addTask}
+        changeStatus={changeStatus}
+        filterTask={filterTask}
       />
     </div>
   );
